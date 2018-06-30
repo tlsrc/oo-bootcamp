9a418259ea4dabf335c7d6a0d2fa86822c59033e
Fred George <fredgeorge@acm.org>
2018-06-30 20:06:49 +0200

    Fred: Added skeleon class definition for Rectangle

---
+++ src/rectangle/Rectangle.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2018 by Fred George
+ * May be used freely except for training; license required for training.
+ * @author Fred George
+ */
+package rectangle
+
+// Understands...
+class Rectangle {
+}
